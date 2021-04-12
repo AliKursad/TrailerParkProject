4ac1eae5556f40208ddaa41ece5df39e15c4cba2
Ali Kürşad Çorapsız <KursadCor@outlook.com>
2021-04-12 23:08:20 +0300

    Add project files.

---
+++ TrailerParkProject/Models/Categories.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrailerParkProject.Models
+{
+    public partial class Categories
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
